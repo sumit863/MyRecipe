eab713ffe280acc323fcbd42655a819d932d9997
Sumit <sumitrajput863@gmail.com>
2015-12-14 07:13:39 +0000

    add pagination

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all   
+    @recipes = Recipe.paginate(page: params[:page] , per_page: 4)
   end
   
   def show
@@ -36,6 +36,17 @@ class RecipesController < ApplicationController
     else
       render :edit
     end
+  end
+  
+  def like
+    @recipe = Recipe.find(params[:id])
+    like = Like.create(like: params[:like] , chef: Chef.first , recipe: @recipe)
+    if like.valid?
+      flash[:success] = "Your selection was Successful."
+    else
+      flash[:danger] = "You can like/dislike a recipe only once."
+    end
+    redirect_to :back
   end
   
   private
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,6 @@
 class Chef < ActiveRecord::Base
   has_many :recipes
+  has_many :likes
   before_save {self.email = email.downcase}
   validates :chefname , presence: true , length: {minimum:3 , maximum:40}
   VALID_EMAIL_REGEX =  /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
