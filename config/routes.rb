6c4a354d0bbbf90aca510cc331d294dbfe969f1a
Sumit <sumitrajput863@gmail.com>
2015-12-17 12:24:09 +0000

    create style and ingredients and their associations

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   
   get '/login' , to: 'logins#new'
   post '/login', to: 'logins#create'
-  get '/logout', to: 'logins#destroy' 
+  get '/logout', to: 'logins#destroy'
+  
+  resources :styles,only: [:new,:create,:show]
+  resources :ingredients,only: [:new,:create,:show]
   
 end
